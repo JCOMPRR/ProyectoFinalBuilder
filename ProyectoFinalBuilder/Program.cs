dc913d6dde87809cf5f034066de4833ca3f13ed9
JCOMPRR <94765491+JCOMPRR@users.noreply.github.com>
2023-10-17 23:37:45 -0700

    Add project files.

---
+++ ProyectoFinalBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProyectoFinalBuilder;
+
+ReporteVentas reporte = new ReporteVentas();
+
+reporte.AgregarTitulo("Reporte de Ventas");
+reporte.AgregarFecha(System.DateTime.Now);
+reporte.AgregarProductos("Monster", 10, 34);
+reporte.AgregarProductos("Cafe", 5, 15);
+
+Console.WriteLine(reporte.GetReporte());
+
---
+++ ProyectoFinalBuilder/ReporteVentas.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace ProyectoFinalBuilder
+{
+    public class ReporteVentas
+    {
+        public string Nombre;
+        public DateTime Fecha;
+        public List<string> Productos = new List<string>();
+        public List<int> Cantidades = new List<int>();
+        public decimal totalVentas;
+
+        public void AgregarTitulo(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void AgregarFecha(DateTime fecha)
+        {
+            Fecha = fecha;
+        }
+
+        public void AgregarProductos(string producto, int cantidad, int precio)
+        {
+            Productos.Add(producto);
+            Cantidades.Add(cantidad);
+            totalVentas += precio * cantidad; 
+        }
+
+        public string GetReporte()
+        {
+            var detalle = new StringBuilder();
+            for (int i = 0; i < Productos.Count; i++)
+            {
+                detalle.AppendLine($"{Productos[i]} - {Cantidades[i]}");
+            }
+            var reporte = $"" +
+                $"Titulo: {Nombre}" +
+                $"Fecha: {Fecha}" +
+                $"Detalle: {detalle.ToString()}" +
+                $"Total: {totalVentas}";
+            
+            return reporte;
+        }
+    }
+}
